305a480a1e5c449284745bdc11058d0a8acaaa90
Punam Masal <punammasal2@gmail.com>
2026-07-10 16:10:50 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats =0;
+        int start =0;
+        int end = people.length-1;
+        while(start <= end){
+            if(people[start]+people[end] <= limit){
+                start++;
+                end--;
+            }else{ // heaviest hot asel tr
+                end--;
+            }
+            boats++;
+        }
+        return boats;
+    }
+}
